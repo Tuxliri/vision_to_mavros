53c386691c4038fd9b04c6e8dacb4b99c95549ef
Davide Iafrate <dvde.iafrate98@gmail.com>
2025-04-04 15:52:28 +0000

    Ported script to set EKF and GPS origin to MAVlink 2.0

--- scripts/set_origin.py
+++ scripts/set_origin.py
@@ -11,6 +11,51 @@ os.environ["MAVLINK20"] = "1"
 
 VEHICLE_NAME = os.environ.get("VEHICLE_NAME", "duckiedrone")
 
+def send_message(msg, mav, pub):
+    # Explicitly pack the message
+    mav = MAV_APM.MAVLink(None, srcSystem=1)
+    msg.pack(mav)
+    # Convert MAVLink message to ROS format
+    ros_msg = convert_to_rosmsg(msg)
+    pub.publish(ros_msg)
+    rospy.loginfo("Message sent")
+
+def set_home_position(mav, pub):
+    """
+    Send a MAVLink SET_HOME_POSITION message, which should allow
+    us to use local position information without a GPS
+    """
+    target_system = 0  # broadcast to everyone
+
+    # Use the same home position values as in send_set_gps_global_origin
+    lat = 425633500  # Example latitude in degrees * 1E7
+    lon = 126432900  # Example longitude in degrees * 1E7
+    alt = 163000    # Altitude in mm
+    
+    x = 0
+    y = 0
+    z = 0
+    q = [1, 0, 0, 0]   # w, x, y, z
+
+    approach_x = 0
+    approach_y = 0
+    approach_z = 1
+
+    msg = MAV_APM.MAVLink_set_home_position_message(
+        target_system,
+        lat,
+        lon,
+        alt,
+        x,
+        y,
+        z,
+        q,
+        approach_x,
+        approach_y,
+        approach_z
+    )
+
+    send_message(msg, mav, pub)
 
 def send_set_gps_global_origin():
     rospy.init_node("set_gps_global_origin")
@@ -42,8 +87,10 @@ def send_set_gps_global_origin():
 
     rospy.loginfo("Sending SET_GPS_GLOBAL_ORIGIN message to ")
     mavlink_pub.publish(ros_msg)
-    rospy.sleep(1)
-
+    rospy.loginfo("Sending SET_HOME_POSITION message")
+    set_home_position(mav, mavlink_pub)
+    rospy.loginfo("SET_HOME_POSITION message sent")
+    
 
 if __name__ == "__main__":
     try:
