ffbbfbfaed6ee240960d49f2f5cf2485ae6c14c6
Davide Iafrate <Tuxliri@users.noreply.github.com>
2025-06-27 13:23:44 +0200

    Port vision_to_mavros node to ROS2

--- src/vision_to_mavros.cpp
+++ src/vision_to_mavros.cpp
@@ -1,287 +1,142 @@
-#include <ros/ros.h>
-#include <tf/transform_listener.h>
-#include <tf/transform_broadcaster.h>
-#include <geometry_msgs/Pose.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <nav_msgs/Path.h>
-#include <mavros_msgs/LandingTarget.h>
+#include <memory>
+#include <string>
+#include <cmath>
 
-#include <string.h>
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <mavros_msgs/msg/landing_target.hpp>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Vector3.h>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 
-int main(int argc, char** argv)
+int main(int argc, char ** argv)
 {
-  ros::init(argc, argv, "vision_to_mavros");
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<rclcpp::Node>("vision_to_mavros");
 
-  ros::NodeHandle node("~");
-  
-  //////////////////////////////////////////////////
-  // Variables for precision navigation
-  //////////////////////////////////////////////////
-  ros::Publisher camera_pose_publisher = node.advertise<geometry_msgs::PoseStamped>("vision_pose", 10);
+  // Parameters
+  std::string target_frame_id = node->declare_parameter<std::string>("target_frame_id", "/camera_frame");
+  std::string source_frame_id = node->declare_parameter<std::string>("source_frame_id", "/camera_link");
+  double output_rate = node->declare_parameter<double>("output_rate", 20.0);
+  double roll_cam = node->declare_parameter<double>("roll_cam", 0.0);
+  double pitch_cam = node->declare_parameter<double>("pitch_cam", 0.0);
+  double yaw_cam = node->declare_parameter<double>("yaw_cam", 1.5707963);
+  double gamma_world = node->declare_parameter<double>("gamma_world", -1.5707963);
 
-  ros::Publisher body_path_pubisher = node.advertise<nav_msgs::Path>("body_frame/path", 1);
+  bool enable_precland = node->declare_parameter<bool>("enable_precland", false);
+  std::string precland_target_frame_id = node->declare_parameter<std::string>("precland_target_frame_id", "/landing_target");
+  std::string precland_camera_frame_id = node->declare_parameter<std::string>("precland_camera_frame_id", "/camera_fisheye2_optical_frame");
 
-  tf::TransformListener tf_listener;
-  tf::TransformBroadcaster tf_broadcaster;
-  tf::Transform tf_body;
-
-  tf::StampedTransform transform;
-
-  geometry_msgs::PoseStamped msg_body_pose;
-
-  nav_msgs::Path body_path;
-
-  std::string target_frame_id = "/camera_frame";
-
-  std::string source_frame_id = "/camera_link";
-
-  double output_rate = 20, roll_cam = 0, pitch_cam = 0, yaw_cam = 1.5707963, gamma_world = -1.5707963;
-
-  // Read parameters from launch file, including: target_frame_id, source_frame_id, output_rate
-  {
-    // The frame in which we find the transform into, the original "world" frame
-    if(node.getParam("target_frame_id", target_frame_id))
-    {
-      ROS_INFO("Get target_frame_id parameter: %s", target_frame_id.c_str());
-    }
-    else
-    {
-      ROS_WARN("Using default target_frame_id: %s", target_frame_id.c_str());
-    }
-
-    // The frame for which we find the tranform to target_frame_id, the original "camera" frame
-    if(node.getParam("source_frame_id", source_frame_id))
-    {
-      ROS_INFO("Get source_frame_id parameter: %s", source_frame_id.c_str());
-    }
-    else
-    {
-      ROS_WARN("Using default source_frame_id: %s", source_frame_id.c_str());
-    }
-
-    // The rate at which we wish to publish final pose data
-    if(node.getParam("output_rate", output_rate))
-    {
-      ROS_INFO("Get output_rate parameter: %f", output_rate);
-    }
-    else
-    {
-      ROS_WARN("Using default output_rate: %f", output_rate);
-    }
-
-    // The rotation around z axis between original world frame and target world frame, assuming the z axis needs not to be changed
-    // In this case, target world frame has y forward, x to the right and z upwards (ENU as ROS dictates)
-    if(node.getParam("gamma_world", gamma_world))
-    {
-      ROS_INFO("Get gamma_world parameter: %f", gamma_world);
-    }
-    else
-    {
-      ROS_WARN("Using default gamma_world: %f", gamma_world);
-    }
-
-    // The roll angle around camera's own axis to align with body frame 
-    if(node.getParam("roll_cam", roll_cam))
-    {
-      ROS_INFO("Get roll_cam parameter: %f", roll_cam);
-    }
-    else
-    {
-      ROS_WARN("Using default roll_cam: %f", roll_cam);
-    }
-
-    // The pitch angle around camera's own axis to align with body frame 
-    if(node.getParam("pitch_cam", pitch_cam))
-    {
-      ROS_INFO("Get pitch_cam parameter: %f", pitch_cam);
-    }
-    else
-    {
-      ROS_WARN("Using default pitch_cam: %f", pitch_cam);
-    }
-
-    // The yaw angle around camera's own axis to align with body frame 
-    if(node.getParam("yaw_cam", yaw_cam))
-    {
-      ROS_INFO("Get yaw_cam parameter: %f", yaw_cam);
-    }
-    else
-    {
-      ROS_WARN("Using default yaw_cam: %f", yaw_cam);
-    }
+  auto camera_pose_publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("vision_pose", 10);
+  auto body_path_pubisher = node->create_publisher<nav_msgs::msg::Path>("body_frame/path", 1);
+  rclcpp::Publisher<mavros_msgs::msg::LandingTarget>::SharedPtr precland_msg_publisher;
+  if (enable_precland) {
+    precland_msg_publisher = node->create_publisher<mavros_msgs::msg::LandingTarget>("landing_raw", 10);
   }
 
-  //////////////////////////////////////////////////
-  // Variables for precision landing (optional)
-  //////////////////////////////////////////////////
-  bool enable_precland = false;
+  tf2_ros::Buffer tf_buffer(node->get_clock());
+  tf2_ros::TransformListener tf_listener(tf_buffer);
+  tf2_ros::TransformBroadcaster tf_broadcaster(node);
 
-  std::string precland_target_frame_id = "/landing_target";
+  geometry_msgs::msg::PoseStamped msg_body_pose;
+  nav_msgs::msg::Path body_path;
 
-  std::string precland_camera_frame_id = "/camera_fisheye2_optical_frame";
+  rclcpp::Time last_tf_time = node->now();
+  rclcpp::Time last_precland_tf_time = node->now();
 
-  ros::Publisher precland_msg_publisher;
+  rclcpp::Rate rate(output_rate);
 
-  if(node.getParam("enable_precland", enable_precland))
-  {
-    ROS_INFO("Precision landing: %s", enable_precland ? "enabled" : "disabled");
-  }
-  else
-  {
-    ROS_INFO("Precision landing disabled by default");
-  }
+  while (rclcpp::ok()) {
+    // Use latest available transform
+    geometry_msgs::msg::TransformStamped transform;
+    try {
+      transform = tf_buffer.lookupTransform(target_frame_id, source_frame_id, tf2::TimePointZero);
+      if (last_tf_time < transform.header.stamp) {
+        last_tf_time = transform.header.stamp;
 
-  if (enable_precland)
-  {
-    // The frame of the landing target in the camera frame
-    if(node.getParam("precland_target_frame_id", precland_target_frame_id))
-    {
-      ROS_INFO("Get precland_target_frame_id parameter: %s", precland_target_frame_id.c_str());
-    }
-    else
-    {
-      ROS_WARN("Using default precland_target_frame_id: %s", precland_target_frame_id.c_str());
-    }
+        tf2::Vector3 position_orig(transform.transform.translation.x,
+                                   transform.transform.translation.y,
+                                   transform.transform.translation.z);
+        tf2::Vector3 position_body;
+        position_body.setX(std::cos(gamma_world) * position_orig.x() + std::sin(gamma_world) * position_orig.y());
+        position_body.setY(-std::sin(gamma_world) * position_orig.x() + std::cos(gamma_world) * position_orig.y());
+        position_body.setZ(position_orig.z());
 
-    if(node.getParam("precland_camera_frame_id", precland_camera_frame_id))
-    {
-      ROS_INFO("Get precland_camera_frame_id parameter: %s", precland_camera_frame_id.c_str());
-    }
-    else
-    {
-      ROS_WARN("Using default precland_camera_frame_id: %s", precland_camera_frame_id.c_str());
-    }
-
-    precland_msg_publisher = node.advertise<mavros_msgs::LandingTarget>("landing_raw", 10);
-  }
-
-  //////////////////////////////////////////////////
-  // Wait for the first transform to become available.
-  //////////////////////////////////////////////////
-  tf_listener.waitForTransform(target_frame_id, source_frame_id, ros::Time::now(), ros::Duration(3.0));
-
-  ros::Time last_tf_time = ros::Time::now();
-  ros::Time last_precland_tf_time = ros::Time::now();
-
-  // Limit the rate of publishing data, otherwise the other telemetry port might be flooded
-  ros::Rate rate(output_rate);
-
-  while (node.ok())
-  {
-    // For tf, Time(0) means "the latest available" transform in the buffer.
-    ros::Time now = ros::Time(0);
-
-    //////////////////////////////////////////////////
-    // Publish vision_position_estimate message if transform is available
-    //////////////////////////////////////////////////
-    try
-    {
-      // lookupTransform(frame_2, frame_1, at_this_time, this_transform)
-      //    will give the transfrom from frame_1 to frame_2
-      tf_listener.lookupTransform(target_frame_id, source_frame_id, now, transform);
-
-      // Only publish pose messages when we have new transform data.
-      if (last_tf_time < transform.stamp_)
-      {
-        last_tf_time = transform.stamp_;
-
-        static tf::Vector3 position_orig, position_body;
-
-        static tf::Quaternion quat_cam, quat_cam_to_body_x, quat_cam_to_body_y, quat_cam_to_body_z, quat_rot_z, quat_body;
-
-        // 1) Rotation from original world frame to world frame with y forward.
-        // See the full rotation matrix at https://en.wikipedia.org/wiki/Rotation_matrix#Basic_rotations
-        position_orig = transform.getOrigin();
-
-        position_body.setX( cos(gamma_world) * position_orig.getX() + sin(gamma_world) * position_orig.getY());
-        position_body.setY(-sin(gamma_world) * position_orig.getX() + cos(gamma_world) * position_orig.getY());
-        position_body.setZ(position_orig.getZ());
-
-        // 2) Rotation from camera to body frame.
-        quat_cam = transform.getRotation();
-
-        quat_cam_to_body_x = tf::createQuaternionFromRPY(roll_cam, 0, 0);
-        quat_cam_to_body_y = tf::createQuaternionFromRPY(0, pitch_cam, 0);
-        quat_cam_to_body_z = tf::createQuaternionFromRPY(0, 0, yaw_cam);
-        
-        // 3) Rotate body frame 90 degree (align body x with world y at launch)
-        quat_rot_z = tf::createQuaternionFromRPY(0, 0, -gamma_world);
-
+        tf2::Quaternion quat_cam(transform.transform.rotation.x,
+                                 transform.transform.rotation.y,
+                                 transform.transform.rotation.z,
+                                 transform.transform.rotation.w);
+        tf2::Quaternion quat_cam_to_body_x, quat_cam_to_body_y, quat_cam_to_body_z, quat_rot_z, quat_body;
+        quat_cam_to_body_x.setRPY(roll_cam, 0, 0);
+        quat_cam_to_body_y.setRPY(0, pitch_cam, 0);
+        quat_cam_to_body_z.setRPY(0, 0, yaw_cam);
+        quat_rot_z.setRPY(0, 0, -gamma_world);
         quat_body = quat_rot_z * quat_cam * quat_cam_to_body_x * quat_cam_to_body_y * quat_cam_to_body_z;
         quat_body.normalize();
 
-        // Create PoseStamped message to be sent
-        msg_body_pose.header.stamp = transform.stamp_;
-        msg_body_pose.header.frame_id = transform.frame_id_;
-        msg_body_pose.pose.position.x = position_body.getX();
-        msg_body_pose.pose.position.y = position_body.getY();
-        msg_body_pose.pose.position.z = position_body.getZ();
-        msg_body_pose.pose.orientation.x = quat_body.getX();
-        msg_body_pose.pose.orientation.y = quat_body.getY();
-        msg_body_pose.pose.orientation.z = quat_body.getZ();
-        msg_body_pose.pose.orientation.w = quat_body.getW();
+        msg_body_pose.header.stamp = transform.header.stamp;
+        msg_body_pose.header.frame_id = transform.header.frame_id;
+        msg_body_pose.pose.position.x = position_body.x();
+        msg_body_pose.pose.position.y = position_body.y();
+        msg_body_pose.pose.position.z = position_body.z();
+        msg_body_pose.pose.orientation.x = quat_body.x();
+        msg_body_pose.pose.orientation.y = quat_body.y();
+        msg_body_pose.pose.orientation.z = quat_body.z();
+        msg_body_pose.pose.orientation.w = quat_body.w();
 
-        // Publish pose of body frame in world frame
-        camera_pose_publisher.publish(msg_body_pose);
+        camera_pose_publisher->publish(msg_body_pose);
 
-        // Publish trajectory path for visualization
         body_path.header.stamp = msg_body_pose.header.stamp;
         body_path.header.frame_id = msg_body_pose.header.frame_id;
         body_path.poses.push_back(msg_body_pose);
-        body_path_pubisher.publish(body_path);
+        body_path_pubisher->publish(body_path);
 
-        tf_body.setOrigin(position_body);
-        tf_body.setRotation(quat_body);
-        tf_broadcaster.sendTransform(tf::StampedTransform(tf_body, transform.stamp_, transform.frame_id_, "body_frame"));
+        geometry_msgs::msg::TransformStamped tf_msg;
+        tf_msg.header.stamp = transform.header.stamp;
+        tf_msg.header.frame_id = transform.header.frame_id;
+        tf_msg.child_frame_id = "body_frame";
+        tf_msg.transform.translation.x = position_body.x();
+        tf_msg.transform.translation.y = position_body.y();
+        tf_msg.transform.translation.z = position_body.z();
+        tf_msg.transform.rotation.x = quat_body.x();
+        tf_msg.transform.rotation.y = quat_body.y();
+        tf_msg.transform.rotation.z = quat_body.z();
+        tf_msg.transform.rotation.w = quat_body.w();
+        tf_broadcaster.sendTransform(tf_msg);
       }
-    }
-    catch (tf::TransformException ex)
-    {
-      ROS_WARN("%s",ex.what());
-      ros::Duration(1.0).sleep();
+    } catch (const tf2::TransformException & ex) {
+      RCLCPP_WARN(node->get_logger(), "%s", ex.what());
     }
 
-    //////////////////////////////////////////////////
-    // Publish landing_target message if option is enabled and transform is available 
-    //////////////////////////////////////////////////
-    if (enable_precland)
-    {
-      if (tf_listener.canTransform(precland_camera_frame_id, precland_target_frame_id, now))
-      {
-        // lookupTransform(frame_2, frame_1, at_this_time, this_transform)
-        //    will give the transfrom from frame_1 to frame_2
-        tf_listener.lookupTransform(precland_camera_frame_id, precland_target_frame_id, now, transform);
+    if (enable_precland) {
+      if (tf_buffer.canTransform(precland_camera_frame_id, precland_target_frame_id, tf2::TimePointZero)) {
+        transform = tf_buffer.lookupTransform(precland_camera_frame_id, precland_target_frame_id, tf2::TimePointZero);
+        if (last_precland_tf_time < transform.header.stamp) {
+          last_precland_tf_time = transform.header.stamp;
 
-        // Only publish when we have new data
-        if (last_precland_tf_time < transform.stamp_)
-        {
-          last_precland_tf_time = transform.stamp_;
-
-          mavros_msgs::LandingTarget msg_landing_target;
-
-          // Setup the landing target message according to the relative protocol: https://mavlink.io/en/services/landing_target.html#camera_image_relative
-          msg_landing_target.header.frame_id = transform.frame_id_;
-          msg_landing_target.header.stamp = transform.stamp_;
+          mavros_msgs::msg::LandingTarget msg_landing_target;
+          msg_landing_target.header.frame_id = transform.header.frame_id;
+          msg_landing_target.header.stamp = transform.header.stamp;
           msg_landing_target.target_num = 0;
-          msg_landing_target.frame = mavros_msgs::LandingTarget::LOCAL_NED;
-          msg_landing_target.type = mavros_msgs::LandingTarget::VISION_FIDUCIAL;
-
-          msg_landing_target.angle[0] = std::atan(transform.getOrigin().getX() / transform.getOrigin().getZ());
-          msg_landing_target.angle[1] = std::atan(transform.getOrigin().getY() / transform.getOrigin().getZ());
-          msg_landing_target.distance = transform.getOrigin().length();
-
-          // Publish the message
-          precland_msg_publisher.publish(msg_landing_target);
-
-          ROS_INFO("Landing target detected");
+          msg_landing_target.frame = mavros_msgs::msg::LandingTarget::LOCAL_NED;
+          msg_landing_target.type = mavros_msgs::msg::LandingTarget::VISION_FIDUCIAL;
+          msg_landing_target.angle[0] = std::atan2(transform.transform.translation.x, transform.transform.translation.z);
+          msg_landing_target.angle[1] = std::atan2(transform.transform.translation.y, transform.transform.translation.z);
+          msg_landing_target.distance = std::sqrt(std::pow(transform.transform.translation.x,2)+std::pow(transform.transform.translation.y,2)+std::pow(transform.transform.translation.z,2));
+          precland_msg_publisher->publish(msg_landing_target);
+          RCLCPP_INFO(node->get_logger(), "Landing target detected");
         }
       }
     }
 
-    //////////////////////////////////////////////////
-    // Repeat
-    //////////////////////////////////////////////////
+    rclcpp::spin_some(node);
     rate.sleep();
   }
+
+  rclcpp::shutdown();
   return 0;
 }
+
